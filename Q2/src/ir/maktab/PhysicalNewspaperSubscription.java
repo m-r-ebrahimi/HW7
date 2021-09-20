664cc77dec0e3e1b640d2279422b678035ea0616
MohammadReza8080 <emohammadreza568@gmil.com>
2021-09-20 17:35:57 +0430

    Factor PhysicalNewspaperSubscription class

---
+++ Q2/src/ir/maktab/PhysicalNewspaperSubscription.java
@@ -0,0 +1,30 @@
+package ir.maktab;
+
+public class PhysicalNewspaperSubscription extends NewspaperSubscription{
+
+    @Override
+    public void setAddress(String address) {
+        address = address.trim();
+        if(checkString(address)){
+            super.address=address;
+            setPrice(15);
+        }else{
+            System.out.println("Exception!!!");
+            setPrice(0);
+        }
+    }
+
+    @Override
+    public void setPrice(int price) {
+        super.price=price;
+    }
+
+    private boolean checkString(String str){
+        for (int i = 0; i < str.length(); i++) {
+            if(Character.isLetter(str.charAt(i)))
+                return true;
+        }
+        return false;
+    }
+}
+

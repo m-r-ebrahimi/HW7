a8473dda0bef468850b63cbab1a2c6dd18da6fbc
MohammadReza8080 <emohammadreza568@gmil.com>
2021-09-20 18:13:55 +0430

    Factor Element class and it's body

---
+++ Q3/src/ir/maktab/element/Element.java
@@ -0,0 +1,26 @@
+package ir.maktab.element;
+
+public abstract class Element {
+    protected String symbol;
+    protected int atomicNumber;
+    protected double atomicWeight;
+
+    public Element(String symbol, int atomicNumber, double atomicWeight) {
+        this.symbol = symbol;
+        this.atomicNumber = atomicNumber;
+        this.atomicWeight = atomicWeight;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public int getAtomicNumber() {
+        return atomicNumber;
+    }
+
+    public double getAtomicWeight() {
+        return atomicWeight;
+    }
+    protected abstract void describeElement();
+}

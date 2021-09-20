644051a3534e8e62e2167d6f108b1cfacaa320fc
MohammadReza8080 <emohammadreza568@gmil.com>
2021-09-20 18:14:57 +0430

    Factor subclasses

---
+++ Q3/src/ir/maktab/element/MetalElement.java
@@ -0,0 +1,12 @@
+package ir.maktab.element;
+
+public class MetalElement extends Element {
+    public MetalElement(String symbol, int atomicNumber, double atomicWeight) {
+        super(symbol, atomicNumber, atomicWeight);
+    }
+
+    @Override
+    protected void describeElement() {
+
+    }
+}
---
+++ Q3/src/ir/maktab/element/NonMetalElement.java
@@ -0,0 +1,12 @@
+package ir.maktab.element;
+
+public class NonMetalElement extends Element {
+    public NonMetalElement(String symbol, int atomicNumber, double atomicWeight) {
+        super(symbol, atomicNumber, atomicWeight);
+    }
+
+    @Override
+    protected void describeElement() {
+
+    }
+}

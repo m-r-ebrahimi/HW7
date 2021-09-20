3793c957aa4c897c6143750eecdedb8baa9bbe83
MohammadReza8080 <emohammadreza568@gmil.com>
2021-09-20 18:02:36 +0430

    Add Main class to run program

---
+++ Q2/src/ir/maktab/runner/Main.java
@@ -0,0 +1,10 @@
+package ir.maktab.runner;
+
+import ir.maktab.subscription.DemoSubscriptions;
+
+public class Main {
+    public static void main(String[] args) {
+        DemoSubscriptions demoSubscriptions = new DemoSubscriptions();
+        demoSubscriptions.run();
+    }
+}

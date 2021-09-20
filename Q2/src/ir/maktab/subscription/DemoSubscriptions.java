20de0e324dd659b0037f29238297f76c3ff144f7
MohammadReza8080 <emohammadreza568@gmil.com>
2021-09-20 18:03:10 +0430

    Refactor run and print methods

--- Q2/src/ir/maktab/subscription/DemoSubscriptions.java
+++ Q2/src/ir/maktab/subscription/DemoSubscriptions.java
@@ -1,15 +1,23 @@
-package ir.maktab;
+package ir.maktab.subscription;
 
 public class DemoSubscriptions {
     NewspaperSubscription newspaper_1 = new PhysicalNewspaperSubscription("newspaper_1");
     NewspaperSubscription newspaper_2 = new PhysicalNewspaperSubscription("newspaper_2");
-    NewspaperSubscription newspaper_3 = new PhysicalNewspaperSubscription("newspaper_3");
-    NewspaperSubscription newspaper_4 = new PhysicalNewspaperSubscription("newspaper_4");
+    NewspaperSubscription newspaper_3 = new OnlineNewspaperSubscription("newspaper_3");
+    NewspaperSubscription newspaper_4 = new OnlineNewspaperSubscription("newspaper_4");
 
     public void run() {
         newspaper_1.setAddress("ave_1, bank street");
         newspaper_2.setAddress("ave_s, bank street");
         newspaper_3.setAddress("ave_1, @bank street");
         newspaper_1.setAddress("ave_1, bank street");
+        print();
+    }
+
+    private void print() {
+        System.out.println(newspaper_1);
+        System.out.println(newspaper_2);
+        System.out.println(newspaper_3);
+        System.out.println(newspaper_4);
     }
 }

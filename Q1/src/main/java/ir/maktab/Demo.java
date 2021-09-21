337786ac352a719033496582495aa8afb3d404fd
MohammadReza8080 <emohammadreza568@gmil.com>
2021-09-21 10:16:28 +0430

    refactor classes to run and execute.

--- Q1/src/main/java/ir/maktab/Demo.java
+++ Q1/src/main/java/ir/maktab/Demo.java
@@ -1,7 +1,26 @@
 package ir.maktab;
 
-public class Demo {
-    public static void run(){
+import java.util.Scanner;
 
+public class Demo {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void run() {
+        int select;
+        do {
+            System.out.println("""
+                    ==============================
+                    1)See Students
+                    2)See Masters
+                    3)Add Student
+                    4)Add Master
+                    5)Delete Student
+                    6)Delete Master
+                    7)Update Student
+                    8)Update Master
+                    0)EXIT
+                    """);
+            select=scanner.nextInt();
+        } while (select != 0);
     }
 }
